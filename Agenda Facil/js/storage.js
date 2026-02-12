156df898412a1fc35fb8be00204feab4c3b47b7b
beckeracessoriadearmas-max <beckeracessoriadearmas@gmail.com>
2026-02-12 16:26:03 -0300

    Delete Agenda Facil directory

--- Agenda Facil/js/storage.js
+++
@@ -1,9 +0,0 @@
-function salvarDiaria(dados){
-let lista=JSON.parse(localStorage.getItem("diarias"))||[];
-lista.push(dados);
-localStorage.setItem("diarias",JSON.stringify(lista));
-}
-
-function pegarDiarias(){
-return JSON.parse(localStorage.getItem("diarias"))||[];
-}
