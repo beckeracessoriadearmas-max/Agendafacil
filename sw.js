a7b5e097a8eb6193f605572fff318e311b72d0c1
beckeracessoriadearmas-max <beckeracessoriadearmas@gmail.com>
2026-02-20 11:27:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install",e=>{
 self.skipWaiting();
 });
-const CACHE_NAME = "agenda-facil-v1";
+const CACHE_NAME = "agenda-facil-v2";
 
 const urlsToCache = [
     "/",
@@ -20,5 +20,19 @@ self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
         .then(response => response || fetch(event.request))
+    );
+
+});
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
     );
 });
